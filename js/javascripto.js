5e9d131f1889787666bbe34b459596b4c01af676
Ry MV <91206010+rymv07@users.noreply.github.com>
2022-07-08 15:59:12 +0800

    updated

--- js/javascripto.js
+++ js/javascripto.js
@@ -1,10 +1,18 @@
 //alert('Hi! Welcome to my Page!');
 
+var mybutton = document.getElementById("myBtn");
 
-function openNav() {
-document.getElementById("mySidenav").style.width = "220px";
-}
+window.onscroll = function() {scrollFunction()};
 
-function closeNav() {
-document.getElementById("mySidenav").style.width = "30px";
-}
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
